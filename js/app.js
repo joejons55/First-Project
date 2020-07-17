f20d505a1ec915269c1410e43d38b9025765f82b
joejons55 <vladomel55@gmail.com>
2020-07-17 14:40:08 -0700

    So far not so good..game is working somehow, but not how Im expecting..buttons are not working well..trying to figure out what is wrong...

--- js/app.js
+++ js/app.js
@@ -1,16 +1,19 @@
 // I will connect a board with cells with js file
 let main = document.querySelector(".main")//Im going to use it later
 // Create Score Container
-let scoreContainer = document.createElement("div")
-scoreContainer.setAttribute("class", "scoreContainer")
-document.body.appendChild(scoreContainer)
-//Add button to start the game
-// const startBtn = document.querySelector('.s')
-// startBtn.addEventListener("click", starGame)
+// Add score to this game
+const scroeEle = document.getElementById("score");
+const levelEle = document.getElementById("level");
+// Add button to start the game and to reset
+const startBtn = document.querySelector('.s')
+const restartBtn = document.querySelector('.r')
+const pauseBtn = document.querySelector('.p')
+const gameOver = document.querySelector(".game-over")
+
 // Game Speed
-let gameSpeed = 500
+// let gameSpeed = 500
 // Array with arrays that will store differen values and single array
-let singleArr = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+let newLine = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 let gameBoard = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
@@ -34,12 +37,38 @@ let gameBoard = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],   
 ]
 
-// Need 
-// let game = {
-//     // Player Score
+//Simply crating player score and levels, timer
+let score = 0;
+let gameTimer;
+let currentLevel = 1;
+let levels = {
+  1: {
+    scorePerLine: 10,
+    speed: 600,
+    nextLevelScore: 20,
+  },
+  2: {
+    scorePerLine: 15,
+    speed: 400,
+    nextLevelScore: 500,
+  },
+  3: {
+    scorePerLine: 20,
+    speed: 300,
+    nextLevelScore: 1000,
+  },
+  4: {
+    scorePerLine: 30,
+    speed: 200,
+    nextLevelScore: 2000,
+  },
+  5: {
+    scorePerLine: 50,
+    speed: 100,
+    nextLevelScore: 5000,
+  },
+};
 
-//     playerScore: 0,
-//     // CPU Score
 
 //Need to create a function that will check each array(y) in array(gameBoard) and each element(x) in this all arras(y). After check it will return
 function draw() {
@@ -58,6 +87,8 @@ function draw() {
     main.innerHTML = mainInnerHTML
 }
 
+
+//Function that changing figure value form 1 to 0
 function removeActiveFigure() {
     for (let y = 0;y < gameBoard.length;y++) {
         for (let x = 0;x < gameBoard[y].length;x++){
@@ -68,106 +99,19 @@ function removeActiveFigure() {
     }
 }
 
-// Since I created one function that can check how new figure is moving, I dont need all these functions
-
-// //Need to write a code that will move DOWN figures on my board
-// function moveTetrDown() {
-//     if (canMoveTetrDown()) { // Calling a canMoveTetrDown function in moveTetrDown function
-//     for (let y = gameBoard.length - 1;y >= 0 ;y--){
-//         for (let x = 0;x < gameBoard[y].length;x++){
-//            if (gameBoard[y][x] === 1){
-//                gameBoard[y + 1][x] = 1
-//                gameBoard[y][x] = 0
-//            } 
-//            }
-//         } 
-//     } else {
-//         fixTetro() // calling this function in this position so it will not show any errors when it reach the bottom
-//     }
-// }
-
-// //Need to write a code that will check if figure need to move after reach the bottom 
-// function canMoveTetrDown() {
-//     for (let y = 0;y < gameBoard.length;y++){
-//         for (let x = 0;x < gameBoard[y].length;x++){ 
-//            if (gameBoard[y][x] === 1){
-//             if(y === gameBoard.length - 1 || gameBoard[y +1][x] === 2) {
-//                 return false
-//             }
-//            }
-//         }
-//     }
-//     return true
-// }
-
-// // Now I can write a function that will move LEFT figures on my board
-// function canTetrMoveLeft() {
-//     for (let y = 0; y < gameBoard.length; y++) {
-//       for (let x = 0; x < gameBoard[y].length; x++) {
-//         if (gameBoard[y][x] === 1) {
-//           if (x === 0 || gameBoard[y][x - 1] === 2) {
-//             return false;
-//           }
-//         }
-//       }
-//     }
-//     return true;
-//   }
-  
-//   function moveTetrLeft() {
-//     if (canTetrMoveLeft()) {
-//       for (let y = gameBoard.length - 1; y >= 0; y--) {
-//         for (let x = 0; x < gameBoard[y].length; x++) {
-//           if (gameBoard[y][x] === 1) {
-//             gameBoard[y][x - 1] = 1;
-//             gameBoard[y][x] = 0;
-//           }
-//         }
-//       }
-//     }
-//   }
-
-// //Now I can write a function that will move Right figures on my board
-// function moveTetrRight() {
-//     if (canMoveTetrRight()) { 
-//     for (let y = gameBoard.length - 1;y >= 0 ;y--){
-//         for (let x = 9;x >= 0;x--){
-//            if (gameBoard[y][x] === 1){
-//                gameBoard[y][x + 1] = 1
-//                gameBoard[y][x] = 0 //When it reach all the way to the right it will not move further
-//            } 
-//            }
-//         } 
-    
-//     } 
-// }
-
-// function canMoveTetrRight() {
-//     for (let y = 0;y < gameBoard.length;y++){
-//         for (let x = 0;x < gameBoard[y].length;x++){
-//            if (gameBoard[y][x] === 1){
-//             if(x === 9 || gameBoard[y][x + 1] === 2) {
-//                 return false
-//             }
-//            }
-//         }
-//     }
-//     return true
-// }
-
-// Need to create a function that will update board with new figure
+// Need to create a function that will update active figure value from 1 to 0. So when figure changig it location it will not stay on the previous 
 function updateActiveFigure() {
     removeActiveFigure()
-    for (let y = 0; y < newFigure.shape.length; y++) {
-        for (let x = 0; x <newFigure.shape[y].length; x++) {
-            if (newFigure.shape[y][x]) {
-                gameBoard[newFigure.y + y][newFigure.x + x] = newFigure.shape[y][x]
+    for (let y = 0; y < mainFigure.shape.length; y++) {
+        for (let x = 0; x <mainFigure.shape[y].length; x++) {
+            if (mainFigure.shape[y][x] === 1) {
+                gameBoard[mainFigure.y + y][mainFigure.x + x] = mainFigure.shape[y][x]
             }
         }
     }
 }
 
-//Need to write a function with a check that will stop figure when its meet anothr figure or rach the bottom, and will call a new figure
+//Need to write a function with a check that will stop figure when its meet anothr figure or reach the bottom, and changing value to 2
 function fixTetro(){
     for (let y = 0;y < gameBoard.length;y++){
         for (let x = 0;x < gameBoard[y].length;x++){
@@ -176,39 +120,14 @@ function fixTetro(){
             }
         }
     }
-    removeLine() //calling this function to remove last line 
-
-    gameBoard[0] = [0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
-    gameBoard[1] = [0, 0, 0, 0, 1, 1, 0, 0, 0, 0]
 }
 
-//Need to create a new objects, and inside those objects need to create arrays, so after I can creat a function that will randomize those arrays
-let newFigure = {
-    x: 0,
-    y: 0,
-    shape: [
-        [1, 1, 1], 
-        [0, 1, 0], 
-        [0, 0, 0]
-    ],
-}
 
-//Function that will check if new figure is not going out of game board
-function hasCollision() {
-    for (let y = 0; y < newFigure.shape.length; y++) {
-        for (let x = 0; x <newFigure.shape[y].length; x++) {
-            if ((newFigure.shape[y][x] && gameBoard[newFigure.y + y] === undefined) ||
-                (gameBoard[newFigure.y + y][newFigure.x + x] === undefined)) {
-                return true
-            }
-        }
-    }
-    return false
-}
 
-// Now I need to make a function that will help me to check if whole line was filled out and then delete this line from the board
+// Now I need to make a function that will help me to check if whole line was filled out and then delete this line from the board and will update old row with new empty one
 function removeLine() {
     let remove = true
+    filledLines = 0
     for (let y = 0; y < gameBoard.length; y++) {
         for (let x = 0; x < gameBoard[y].length; x++) {
             if (gameBoard[y][x] !== 2) {
@@ -218,51 +137,255 @@ function removeLine() {
         }
     if (remove) {
         gameBoard.splice(y, 1) // by it self is not working, need to add another splice to return back new line
-        gameBoard.splice(0, 0, singleArr)// startin with index 0, Im deliting 0 element and addin new line(array)
+        gameBoard.splice(0, 0, newLine)// startin with index 0, Im deliting 0 element and addin new line(array)
+        filledLines +=1
     }
     remove = true
     }
+    // So now removeLine function will remove filled row and add a score
+    switch (filledLines) {
+        case 1:
+          score += levels[currentLevel].scorePerLine;
+          break;
+        case 2:
+          score += levels[currentLevel].scorePerLine * 2;
+          break;
+        case 3:
+          score += levels[currentLevel].scorePerLine * 4;
+          break;
+        case 4:
+          score += levels[currentLevel].scorePerLine * 8;
+          break;
+      }
+    
+      scroeEle.innerHTML = score;
+    
+      if (score >= levels[currentLevel].nextLevelScore) {
+        currentLevel++;
+        levelEle.innerHTML = currentLevel;
+      }
 }
 
-// function startGame() {
-//     moveTetrDown()
-//     draw()
-//     setTimeout(startGame, gameSpeed)
-// } 
 
+// Creating all figures for game
+let figures = {
+    O: [
+      [1, 1],
+      [1, 1],
+    ],
+    I: [
+      [0, 0, 0, 0],
+      [1, 1, 1, 1],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+    ],
+    S: [
+      [0, 1, 1],
+      [1, 1, 0],
+      [0, 0, 0],
+    ],
+    Z: [
+      [1, 1, 0],
+      [0, 1, 1],
+      [0, 0, 0],
+    ],
+    J: [
+      [0, 1, 0],
+      [0, 1, 0],
+      [1, 1, 0],
+    ],
+    L: [
+      [0, 1, 0],
+      [0, 1, 0],
+      [0, 1, 1],
+    ],
+    T: [
+      [1, 1, 1], 
+      [0, 1, 0], 
+      [0, 0, 0], 
+    ],
+  };
+
+//   Creating a function that will randomly chose figures from "figures"
+  function getNewFigure() {
+    const possibleFigures = "IOLJTSZ";
+    const rondo = Math.floor(Math.random() * 7);
+    const newFigure = figures[possibleFigures[rondo]];
+  
+    return {
+      x: Math.floor((10 - newFigure[0].length) / 2), //using this calculation Im chosing locotion where new figures start faling...alsomo from the center of the board 
+      y: 0,//from the first line
+      shape: newFigure, //game will start with this figure
+    };
+  }
+
+  //Function that will check if new figure is not going out of game board
+function hasCollision() {
+    for (let y = 0; y < mainFigure.shape.length; y++) {
+        for (let x = 0; x < mainFigure.shape[y].length; x++) {
+            if (mainFigure.shape[y][x] && (gameBoard[mainFigure.y + y] === undefined ||
+                gameBoard[mainFigure.y + y][mainFigure.x + x] === undefined || 
+                gameBoard[mainFigure.y + y][mainFigure.x + x] === 2)) {
+                return true
+            }
+        }
+    }
+    return false
+}
+// Rotation function that doing rotation around mainFigure
+function rotation() {
+    const prevTetroState = mainFigure.shape;
+    mainFigure.shape = mainFigure.shape[0].map((val, index) =>
+      mainFigure.shape.map((row) => row[index]).reverse()
+    );
+    if (hasCollision()) {
+      mainFigure.shape = prevTetroState;
+    }
+  }
+
+  let mainFigure = getNewFigure(); //will switch first figure with new rendomly choosen 
+
+//   This function 
+  function reset() {
+    pausedaused = true;
+    clearTimeout(gameTimer);
+    gameBoard = [
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ];
+    draw();
+    gameOver.style.display = "block";
+  }
+//   This function is working, but because I created "" object and did a lot of checking for this object, it would be to much work to re-write whole code
+// let topT = [[1, 1, 1], 
+//            [0, 1, 0], 
+//            [0, 0, 0]]
+
+// let rightT = [[0, 0, 1], 
+//              [0, 1, 1], 
+//              [0, 0, 1]]
+
+// let buttomT  =[[0, 0, 0], 
+//               [0, 1, 0], 
+//               [1, 1, 1]]
+
+// let leftT = [[1, 0, 0], 
+//             [1, 1, 0], 
+//             [1, 0, 0]]
+
+
+// // Function that will rotate figure
+// function rotation() { 
+//     const prevState = mainFigure.shape
+//     if (JSON.stringify(mainFigure.shape) == JSON.stringify(topT)) {
+//         mainFigure.shape = rightT
+//     } else if (JSON.stringify(mainFigure.shape) == JSON.stringify(rightT)) {
+//         mainFigure.shape = buttomT
+//     } else if (JSON.stringify(mainFigure.shape) == JSON.stringify(buttomT)) {
+//         mainFigure.shape = leftT
+//     }  else if (JSON.stringify(mainFigure.shape) == JSON.stringify(leftT)) {
+//         mainFigure.shape = topT
+//     } if (hasCollision()){
+//         mainFigure.shape = prevState
+//     }
+//     } 
+    
+let paused = true;
 
 // Need to creat buttons and give thaem a value
 let KEY_LEFT = 37;
 let KEY_RIGHT = 39;
 let KEY_DOWN = 40;
-let KEY_A = 65;
-let KEY_D = 68;
-let KEY_SPACE = 32;
+let KEY_R = 82;
 
 
-// Now I need to create property with a "move" function. So I google it and found the solution
-document.onkeydown = function(e) { //creating function with events
-    if(e.keyCode === KEY_LEFT) { // 37 is a button with left arrow on a leptop keyboard
-        newFigure.x -= 1
+// Now I need to create property with a "move" function. So I google it and found the solution on w3shool and mdn
+document.onkeydown = function(ev) { //creating function with events
+    if (!paused)
+    if(ev.keyCode === KEY_LEFT) { // 37 is a button with left arrow on a leptop keyboard
+        mainFigure.x -= 1
         if (hasCollision()) {
-            newFigure.x += 1
+            mainFigure.x += 1
         }
-    } else if (e.keyCode === KEY_RIGHT) {
-        newFigure.x += 1
+    } else if (ev.keyCode === KEY_RIGHT) {
+        mainFigure.x += 1
         if (hasCollision()) {
-            newFigure.x -= 1
+            mainFigure.x -= 1
         }
-    } else if (e.keyCode === KEY_DOWN) {
-        newFigure.y += 1
-        if (hasCollision()) {
-            newFigure.y -= 1
-        }
-    } else if (e.keyCode === KEY_SPACE) {
-        pauseGame()
-    } 
-    updateActiveFigure()
-    draw() //caling this function again to make more smoth move  
+    } else if (ev.keyCode === KEY_DOWN) {
+        pullFigureDown()
+    } else if (ev.keyCode === KEY_R){
+        rotation()
+    }
+    updateGame() 
 }
-updateActiveFigure()
+
+pauseBtn.addEventListener("click", function(ev) {
+    if (ev.target.innerHTML === "Pause Game") {
+      ev.target.innerHTML = "Keep Playing";
+      clearTimeout(gameTimer);
+    } else {
+      ev.target.innerHTML = "Pause Game";
+      gameTimer = setTimeout(startGame, levels[currentLevel].speed);
+    }
+    paused = !paused;
+  });
+  
+  startBtn.addEventListener("click", function(ev) {
+    paused = false
+    gameTimer = setTimeout(startGame, levels[currentLevel]);
+    gameOver.style.display = "none";
+  });
+
+
+scroeEle.innerHTML = score;
+levelEle.innerHTML = currentLevel;
+
+function updateGame() {
+    if (!paused) {
+      updateActiveFigure();
+      draw()
+    }
+  }
+
+
+function pullFigureDown() {
+    mainFigure.y += 1
+    if (hasCollision()) {
+        mainFigure.y -= 1 
+        fixTetro() // so when figure going all the way down, Im calling this function to see that there is no errors and switching value to 2
+        removeLine()
+        mainFigure = getNewFigure()
+        if (hasCollision()) {
+            reset()
+        }
+    }
+}
+
 draw()
-setTimeout(startGame, gameSpeed)
+
+function startGame() {
+    pullFigureDown()
+    if (!paused) {
+    updateGame()
+    gameTimer = setTimeout(startGame, levels[currentLevel].speed)
+    }
+} 
